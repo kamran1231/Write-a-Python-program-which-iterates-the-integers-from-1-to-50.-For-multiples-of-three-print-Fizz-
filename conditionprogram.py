fbe5bc31e26c9882d5c37c8f0f013c20eba51206
kamran khan <48378125+kamran1231@users.noreply.github.com>
2020-03-07 20:32:22 +0530

    Add files via upload

---
+++ conditionprogram.py
@@ -0,0 +1,13 @@
+# Write a Python program which iterates the integers from 1 to 50. For multiples of three print
+# "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are
+# multiples of both three and five print "FizzBuzz"
+
+for i in range(1,51):
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz',i)
+        continue
+    elif i % 3 == 0:
+        print('fizz',i)
+        continue
+    elif i % 5 == 0:
+        print('Buzz',i)
